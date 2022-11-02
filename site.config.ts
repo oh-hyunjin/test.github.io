5608f801c89903b84318af415e13993c8f7366d4
hyunjin <hyunjin6918@naver.com>
2022-11-02 10:15:07 +0900

    a

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd418f87fac8a40d8a1fd7266bf63eb84',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'oh-hyunjin.github.io',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
